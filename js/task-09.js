47ce8a8c4066430eff2e568fa7a153afc493e017
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-06-15 20:08:50 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,6 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const widgetEl = document.querySelector(".widget");
+/*const widgetEl = document.querySelector(".widget");
 const spanColor = document.querySelector(".color");
-const colorChangeBtn = document.querySelector(".change-color");
+const colorChangeBtn = document.querySelector(".change-color");*/
+
+const refs = {
+  bodyStyle: document.querySelector("body").style,
+  buttonChangeColor: document.querySelector("button.change-color"),
+  textColor: document.querySelector("span.color"),
+};
+
+refs.buttonChangeColor.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  refs.bodyStyle.backgroundColor = color;
+  refs.textColor.textContent = color;
+}
