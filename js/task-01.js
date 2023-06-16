6f272fa4805274a31785bd2fa479640999f7dc35
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-06-16 22:19:31 +0300

    updete -01

--- js/task-01.js
+++ js/task-01.js
@@ -10,18 +10,18 @@ const x = [...categoriesEl.children].forEach((el) => {
   console.log("Elements:", totalEl);
 });*/
 
-/*const ul = Array.from(document.querySelector('#categories').children);
+const ul = Array.from(document.querySelector("#categories").children);
 for (let elem of ul) {
-    let category = elem.firstElementChild.textContent;   
-    let quantityElem = elem.lastElementChild.children.length;
-    console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
-};*/
+  let category = elem.firstElementChild.textContent;
+  let quantityElem = elem.lastElementChild.children.length;
+  console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
+}
 
-const categoriesEl = document.querySelector("#categories").children.length;
+/*const categoriesEl = document.querySelector("#categories").children.length;
 console.log(`Number of categories: ${categoriesEl}`);
 
 const arrayTitlesCategoriesRef = document.querySelectorAll("h2");
 arrayTitlesCategoriesRef.forEach((title) => {
   console.log("Category:", title.textContent);
   console.log("Elements:", title.nextElementSibling.children.length);
-});
+});*/
